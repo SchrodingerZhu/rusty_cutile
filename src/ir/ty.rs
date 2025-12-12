8cb577628f21a092afb8afa3039d6a25c7077a08
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-12-12 11:35:30 -0500

    convert to interned types

--- src/ir/ty.rs
+++ src/ir/ty.rs
@@ -1,3 +1,7 @@
+use std::marker::PhantomData;
+
+use dashmap::DashSet;
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum Integer {
     I1,
@@ -22,9 +26,9 @@ pub enum AltFloat {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct Tile {
+pub struct Tile<'a> {
     pub dims: Box<[usize]>,
-    pub elem: Box<Type>,
+    pub elem: Type<'a>,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
@@ -56,27 +60,49 @@ impl Dimension {
 pub struct Shape(pub Box<[Dimension]>);
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct TensorView {
+pub struct TensorView<'a> {
     pub shape: Shape,
     pub strides: Shape,
-    pub ty: Box<Type>,
+    pub ty: Type<'a>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct PartitionView {
+pub struct PartitionView<'a> {
     pub tile_shape: Box<[usize]>,
-    pub original_view: Box<TensorView>,
+    pub original_view: Type<'a>,
     pub dimension_map: Box<[i32]>,
     pub masked: bool
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum Type {
+pub enum TypeInstance<'a> {
     Integer(Integer),
     Float(IEEEFloat),
     AltFloat(AltFloat),
-    Pointer(Box<Self>),
-    Tile(Tile),
-    TensorView(TensorView),
-    PartitionView(PartitionView),
+    Pointer(Type<'a>),
+    Tile(Tile<'a>),
+    TensorView(TensorView<'a>),
+    PartitionView(PartitionView<'a>),
+}
+
+type InvariantLifetime<'a> = PhantomData<*mut &'a ()>;
+
+#[derive(Copy, Clone, Debug, Eq)]
+#[repr(transparent)]
+pub struct Type<'a>(&'a TypeInstance<'a>, InvariantLifetime<'a>);
+
+impl<'a> PartialEq for Type<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(self.0, other.0)
+    }
+}
+
+impl<'a> std::hash::Hash for Type<'a> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        (self.0 as *const TypeInstance).hash(state);
+    }
+}
+
+struct TypeInterner<'a> {
+    set: DashSet<TypeInstance<'a>>,
 }
